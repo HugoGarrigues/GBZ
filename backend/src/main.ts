0fc5ed00dafa06933b65d5146339ef2a589892f7
Hugo Garrigues <hugo.garrigues@ynov.com>
2025-06-01 18:23:51 +0200

    feat(auth): add auth system with JWT token and password hash

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     .setTitle('Backend')
     .setDescription('The Backend API description')
     .setVersion('0.1')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
