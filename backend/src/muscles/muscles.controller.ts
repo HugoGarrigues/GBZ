655af723cc98ceda41f5a2056ab375c55de035b4
Hugo Garrigues <hugo.garrigues@ynov.com>
2025-06-04 09:25:11 +0200

    feat(muscles): init muscles crud

---
+++ backend/src/muscles/muscles.controller.ts
@@ -0,0 +1,51 @@
+// src/muscles/muscles.controller.ts
+import {
+  Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe,
+  UseGuards
+} from '@nestjs/common';
+import { MusclesService } from './muscles.service';
+import { CreateMuscleDto } from './dto/create-muscle.dto';
+import { UpdateMuscleDto } from './dto/update-muscle.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+
+@ApiTags('muscles')
+@Controller('muscles')
+export class MusclesController {
+  constructor(private readonly musclesService: MusclesService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiBearerAuth()
+  create(@Body() createMuscleDto: CreateMuscleDto) {
+    return this.musclesService.create(createMuscleDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.musclesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.musclesService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiBearerAuth()
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateMuscleDto: UpdateMuscleDto,
+  ) {
+    return this.musclesService.update(id, updateMuscleDto);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiBearerAuth()
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.musclesService.remove(id);
+  }
+}
